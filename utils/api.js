2e312031d089ba20f297c6b7a26f7fa5def2226e
tulio <tulio@uppoints.com>
2018-03-25 22:26:15 -0300

    add last score

--- utils/api.js
+++ utils/api.js
@@ -5,6 +5,7 @@ const FLASH_CARDS_KEY = 'FlashCards:deck';
 const decksMock = [
   {
     title: 'React',
+    result: '1/2',
     questions: [
       {
         question: 'What is React?',
@@ -18,6 +19,7 @@ const decksMock = [
   },
   {
     title: 'Javascript',
+    result: '',
     questions: [
       {
         question: 'What is a closure?',
@@ -45,6 +47,7 @@ export const saveDeckTitle = title => AsyncStorage.getItem(FLASH_CARDS_KEY).then
   const decksJSON = JSON.parse(decks) || [];
   const newDeck = {
     title,
+    result: '',
     questions: [],
   };
   const newDeckList = [...decksJSON, newDeck];
@@ -58,6 +61,18 @@ export const saveCard = (title, question) => AsyncStorage.getItem(FLASH_CARDS_KE
     const newDeckList = decksJSON.map((deck) => {
       if (deck.title === title) {
         deck.questions.push(question);
+      }
+      return deck;
+    });
+    AsyncStorage.setItem(FLASH_CARDS_KEY, JSON.stringify(newDeckList));
+  });
+
+export const saveResult = (title, result) => AsyncStorage.getItem(FLASH_CARDS_KEY)
+  .then((decks) => {
+    const decksJSON = JSON.parse(decks) || [];
+    const newDeckList = decksJSON.map((deck) => {
+      if (deck.title === title) {
+        deck.result = result; // eslint-disable-line
       }
       return deck;
     });
